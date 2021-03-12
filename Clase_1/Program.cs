dbac0464f4dba409e6d88dbde3c18f8cb2c965b6
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-03-12 11:39:49 -0300

    Hola mundo Clase 1

---
+++ Clase_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo Vespertino 2!");
+        }
+    }
+}
