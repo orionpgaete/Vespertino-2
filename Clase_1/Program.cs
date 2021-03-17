36049bd69a73702609f13432ab3d9484ae0a4789
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-03-17 19:25:30 -0300

    uso de readline y pedir nombre por consola

--- Clase_1/Program.cs
+++ Clase_1/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
+
 namespace Clase_1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
-
+            Console.WriteLine("Hola Mundo 2021");
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Su nombre es " + nombre);
+            Console.ReadKey();
+        
         }
     }
 }
