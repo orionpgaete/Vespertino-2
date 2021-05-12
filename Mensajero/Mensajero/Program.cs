069f1d9fff15383a4dd97013b2d8ea1bda468379
orionpgaete <pedro.gaete@pgcomputer.cl>
2021-05-12 19:19:21 -0400

    clase martes 11-05

--- Mensajero/Mensajero/Program.cs
+++ Mensajero/Mensajero/Program.cs
@@ -1,8 +1,11 @@
 ﻿using MensajerosModel;
 using MensajerosModel.DAL;
+using ServidorSocketUtils;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,7 +37,36 @@ namespace Mensajero
 
         static void IniciarServidor()
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.Iniciar())
+            {
+                while (true)
+                {
+                    Console.WriteLine("Esperando Cliente...");
+                    Socket cliente = servidor.ObtenerCliente();
+                    Console.WriteLine("Cliente recibido");
+                    ClienteCom clienteCom = new ClienteCom(cliente);
+                    clienteCom.Escribir("Ingrese nombre: ");
+                    string nombre = clienteCom.Leer();
+                    clienteCom.Escribir("Ingrese texto: ");
+                    string texto = clienteCom.Leer();
+                    Mensaje mensaje = new Mensaje()
+                    {
+                        Nombre = nombre,
+                        Texto = texto,
+                        Tipo = "TCP"
+                    };
+                    mensajesDAL.AgregarMensaje(mensaje);
+                    clienteCom.Desconectar();
+                }
 
+
+            }
+            else
+            {
+                Console.WriteLine("Fallo de conexion, no es posible iniciar server en {0} ", puerto);
+            }
         }
         static void Main(string[] args)
         {
@@ -42,7 +74,7 @@ namespace Mensajero
             //2. El puerto tiene que ser configurable en el App.Config
             //3. cuando reciba un cliente, tiene que solicitar a ese cliente el 
             //nombre y el texto, y agregar un nuevo mensaje con el tipo TCP.
-
+          // IniciarServidor();
             while (Menu()) ;
         }
 
